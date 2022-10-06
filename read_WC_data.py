4cc00ee052d1c3b666fbd14b84d04336a6769883
Nitesh Mathur <nim081@utulsa.edu>
2022-10-05 20:51:19 -0500

    Read First Training Data Set from xlsx file

---
+++ read_WC_data.py
@@ -0,0 +1,23 @@
+import pandas as pd 
+import numpy as np 
+
+# Read Past World Cup Winners Excel Data 
+# This will be our training data.
+WC_win_data = pd.read_excel('2007.xlsx')
+# print(WC_win_data)
+
+# Calculate Average Age 
+#age = np.average(India_2007_data
+# print(WC_win_data.columns.values)
+# print(WC_win_data.Age)
+print(WC_win_data['Player_Age'])
+
+# Calculate Average T20I Experience 
+
+# Calculate Average International Experience 
+
+# Calculate Total Number of Left Handy Batters
+
+# Calculate Total Number of Right Handy Batters
+
+# Calculate Total Number of Bowling Options 
