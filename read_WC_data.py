803c10476e130c10dabb1d58fb0ab17569e52db6
Nitesh Mathur <nim081@utulsa.edu>
2022-10-05 21:06:57 -0500

    Compute Metrics

--- read_WC_data.py
+++ read_WC_data.py
@@ -6,18 +6,33 @@ import numpy as np
 WC_win_data = pd.read_excel('2007.xlsx')
 # print(WC_win_data)
 
+# Now, calculate the metrics 
+# Note, we use the Ceiling function to round up the average age, average experience, etc. 
+
 # Calculate Average Age 
-#age = np.average(India_2007_data
-# print(WC_win_data.columns.values)
-# print(WC_win_data.Age)
-print(WC_win_data['Player_Age'])
+ave_age = np.ceil(np.average(WC_win_data['Player_Age']))
+print(ave_age)
 
 # Calculate Average T20I Experience 
+ave_T20I_exp = np.ceil(np.average(WC_win_data['T20IExp']))
+print(ave_T20I_exp)
 
 # Calculate Average International Experience 
+ave_int_exp = np.ceil(np.average(WC_win_data['IntExp']))
+print(ave_int_exp)
 
 # Calculate Total Number of Left Handy Batters
+total_left = np.sum(WC_win_data['L'])
+print(total_left)
 
 # Calculate Total Number of Right Handy Batters
+total_right = np.sum(WC_win_data['R'])
+print(total_right)
 
 # Calculate Total Number of Bowling Options 
+total_bowling_options = np.sum(WC_win_data['BO'])
+print(total_bowling_options)
+
+# Calculate Total Number of Wicketkeeping Optiosn 
+total_wicketkeepers = np.sum(WC_win_data['WK'])
+print(total_wicketkeepers)
